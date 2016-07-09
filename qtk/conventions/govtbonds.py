6a900cc11b2cfa330669c4167afe460cc8caeb10
Gouthaman Balaraman <gouthambs@gmail.com>
2016-07-08 22:44:27 -0700

    Should translate convention to id.

--- qtk/conventions/govtbonds.py
+++ qtk/conventions/govtbonds.py
@@ -8,15 +8,15 @@ _govt_bonds = dict.fromkeys([
     "USD."+T.INST_GOVT_BOND.id,
     "USD."+T.CRV_INST_GOVT_BOND.id
 ], {
-    F.COUPON_FREQ: ql.Period(6, ql.Months),
-    F.ACCRUAL_BASIS: ql.ActualActual(ql.ActualActual.Bond),
-    F.END_OF_MONTH: True,
-    F.COMPOUNDING: ql.Compounded,
-    F.SETTLEMENT_DAYS: 1,
-    F.PAYMENT_CALENDAR: ql.UnitedStates(),
-    F.PAYMENT_DAY_CONVENTION: ql.Following,
-    F.SETTLEMENT_CALENDAR: ql.UnitedStates(),
-    F.ROUNDING: ql.ClosestRounding(12)
+    F.COUPON_FREQ.id: ql.Period(6, ql.Months),
+    F.ACCRUAL_BASIS.id: ql.ActualActual(ql.ActualActual.Bond),
+    F.END_OF_MONTH.id: True,
+    F.COMPOUNDING.id: ql.Compounded,
+    F.SETTLEMENT_DAYS.id: 1,
+    F.PAYMENT_CALENDAR.id: ql.UnitedStates(),
+    F.PAYMENT_DAY_CONVENTION.id: ql.Following,
+    F.SETTLEMENT_CALENDAR.id: ql.UnitedStates(),
+    F.ROUNDING.id: ql.ClosestRounding(12)
 })
 
 
